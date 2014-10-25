ac7fd3437fc723dbeaac75f1bde67379ac22f1ce
Kenner Kliemann <kenner.hp@gmail.com>
2014-10-25 17:19:34 -0200

    Adding Rails

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+require 'rack-cas/session_store/active_record'
+
+Rails.application.config.session_store :cookie_store, key: '_casino-rack_SSO_session'
+Rails.application.config.rack_cas.session_store = RackCAS::ActiveRecordStore
+
+# Rails.application.config.session_store :rack_cas_active_record_store
